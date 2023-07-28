bdf3dec7262d9fe41ecd81a8b84efb47da3e62d0
Merini Hakim <135359355+SatY213@users.noreply.github.com>
2023-07-28 14:48:28 +0200

    Update lazyLoad.js

--- js/lazyLoad.js
+++ js/lazyLoad.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Lazy-load the slide images when they become visible
 const slides = document.querySelectorAll(".slide");
 const options = {
@@ -19,25 +18,4 @@ const observer = new IntersectionObserver((entries, observer) => {
 slides.forEach((slide) => {
   observer.observe(slide);
 });
-=======
-// Lazy-load the slide images when they become visible
-const slides = document.querySelectorAll(".slide");
-const options = {
-  threshold: 0.1,
-};
 
-const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      const img = entry.target.querySelector("img");
-      const src = img.getAttribute("data-src");
-      img.setAttribute("src", src);
-      observer.unobserve(entry.target);
-    }
-  });
-}, options);
-
-slides.forEach((slide) => {
-  observer.observe(slide);
-});
->>>>>>> d2f1e5e02a79d26231082864801eaf85738ea326
