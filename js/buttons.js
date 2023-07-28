d1452337af7cd388c9542d902fc9d2a17891cc36
Merini Hakim <135359355+SatY213@users.noreply.github.com>
2023-07-28 14:36:43 +0200

    Update buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // JavaScript code for auto-scrolling to the answer section
 document.addEventListener("DOMContentLoaded", function () {
   var questionButtons = document.querySelectorAll(".question");
@@ -20,24 +19,4 @@ function music() {
   audio.play();
   audio.scrollIntoView({ behavior: "smooth" });
 }
-=======
-// JavaScript code for auto-scrolling to the answer section
-document.addEventListener("DOMContentLoaded", function () {
-  var questionButtons = document.querySelectorAll(".question");
 
-  questionButtons.forEach(function (button) {
-    button.addEventListener("click", function (event) {
-      event.preventDefault();
-      var answerSection = document.getElementById("answer");
-
-      // Scroll to the answer section using smooth behavior
-      answerSection.scrollIntoView({ behavior: "smooth" });
-    });
-  });
-});
-
-window.addEventListener("load", function () {
-  var audio = document.getElementById("myAudio");
-  audio.play();
-});
->>>>>>> d2f1e5e02a79d26231082864801eaf85738ea326
